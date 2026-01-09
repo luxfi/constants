f6cdd0c0d6291649856dc8756c88f0ad4c4af253
Zach Kelling <z@zeekay.ai>
2026-01-08 22:37:29 -0800

    feat: add AvalancheLocalNetworkID and NetrunnerLocalNetworkID

--- cli.go
+++ cli.go
@@ -12,8 +12,10 @@ const (
 	DevnetWSEndpoint  = "wss://api.lux-dev.network/ext/bc/C/ws"
 
 	// Local network configuration
-	LocalNetworkID       = CustomID // 1337 - same as CustomID
-	LocalNetworkNumNodes = 5
+	LocalNetworkID            = CustomID // 1337 - same as CustomID
+	NetrunnerLocalNetworkID   = CustomID // 1337 - same as CustomID for netrunner
+	AvalancheLocalNetworkID   = 31337    // Avalanche's local network ID
+	LocalNetworkNumNodes      = 5
 
 	// Staking constants
 	MinStakeDuration     = 24 * 14 * time.Hour  // 2 weeks
